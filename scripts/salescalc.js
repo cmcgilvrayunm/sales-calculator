35e8763e33487005ebcc65a83bcca47d6ed68b89
chancemcgilvray <mcgilvray.chance@gmail.com>
2021-02-08 22:01:34 -0700

    Modified code. Fixes #2

--- scripts/salescalc.js
+++ scripts/salescalc.js
@@ -1,9 +1,12 @@
+//adds listener
 addEventListener("load", main)
 
+//creates main function for calculating sales
 function main() {
     btCalcSales.addEventListener("click", calculateSales)
 }
 
+//creates function to actually calculate sales
 function calculateSales() {
     Price=parseFloat(txPrice.value)
     TaxRate=parseFloat(txTaxRate.value)/100
